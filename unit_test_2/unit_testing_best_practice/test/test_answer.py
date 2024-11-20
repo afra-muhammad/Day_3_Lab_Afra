ff6d33b73d4682f8a4e0f898bf8043398afa396e
Afra Muhammad <afra.muhammad@edu.dsti.institute>
2024-11-20 15:15:26 +0100

    SECOND TEST

--- unit_test_2/unit_testing_best_practice/test/test_answer.py
+++ unit_test_2/unit_testing_best_practice/test/test_answer.py
@@ -1,11 +1,7 @@
 import sys
 
-#print(sys.path)
-# Always run from unit_testing_best_practice/test
-
 sys.path += ['..\\src'] 
 
-#sys.path.insert(1, 'C:/Users/Afra Muhammad/unit_testing_best_practice/src')
 
 from sample import *
 
