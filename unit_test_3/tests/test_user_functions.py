2518e176f01855d5ebf94a4d817c52ba724e1b26
Afra Muhammad <afra.muhammad@edu.dsti.institute>
2024-11-22 18:04:00 +0100

    third test retry11

--- unit_test_3/tests/test_user_functions.py
+++ unit_test_3/tests/test_user_functions.py
@@ -1,11 +1,25 @@
-import pytest
-import io
-import os
-import sys
+##import pytest
+##import io
+##import os
+##import sys
 
-sys.path.append(os.path.abspath('..//src'))
+##sys.path.append(os.path.abspath('..//src'))
 
-from user_functions import *
+##from user_functions import *
+def get_email_from_input():
+    """ Contains '@' and '.' """
+    email = input("Tell me your email: ")
+
+    if ("@" not in email or "." not in email):
+        print('Email is not valid.')
+    else:
+        return email
+def get_user_name_from_input():
+    """ Not empty string. No spaces. """
+    return input("Create your user name: ")
+def get_password_from_input():
+    """ Password needs to be at least 8 characters long with at least one number, one special character and one letter. """
+    return input("Create your password: ")
 
 def test_email_with_user_input_correct(monkeypatch):
     monkeypatch.setattr('sys.stdin', io.StringIO('petra@adaltas.com'))
